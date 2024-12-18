5684b4d771ef177423c77d02fb62f226cf6d5eb2
joelriveradev <joelriveradev@gmail.com>
2024-12-18 15:32:57 -0600

    chore: refactor main

--- main.ts
+++ main.ts
@@ -1,25 +1,10 @@
-import { WatcherService } from './watcher.ts'
-import { FTPConfig } from './types.ts'
+import { createWatcher } from '@/watcher.ts'
 
-async function run() {
-  const config: FTPConfig = {
-    host: '127.0.0.1',
-    port: 2121,
-    username: 'testuser',
-    password: 'password123',
-    directory: '850',
-  }
-
+async function init() {
   const kv = await Deno.openKv()
-  const watcher = new WatcherService(config, kv)
 
-  try {
-    await watcher.connect()
-    await watcher.start()
-  } catch (error) {
-    console.error('Error:', error)
-    await watcher.stop()
-  }
+  const { start } = createWatcher()
+  await start(kv)
 }
 
-run()
+init()
